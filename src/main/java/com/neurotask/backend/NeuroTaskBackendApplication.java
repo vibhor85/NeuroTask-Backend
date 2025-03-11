9f11f02b4ee20dbd822269b5149d1109c228dac7
Vibhor <vibhork85@gmail.com>
2025-03-11 22:31:20 +0530

    Add initial project structure and configuration files

---
+++ src/main/java/com/neurotask/backend/NeuroTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neurotask.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroTaskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neurotask/backend/NeuroTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurotask.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
