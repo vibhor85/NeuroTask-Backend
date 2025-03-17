9402196757ce4e4e2f4ab82640530bfac89eb971
Vibhor <vibhork85@gmail.com>
2025-03-17 12:26:48 +0530

    initial setup

---
+++ src/main/java/com/neurotask/backend/NeuroTaskApplication.java
@@ -0,0 +1,13 @@
+package com.neurotask.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neurotask/backend/NeuroTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurotask.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
